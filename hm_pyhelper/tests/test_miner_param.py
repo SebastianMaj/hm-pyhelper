001bdf5d0f2d902222580f1325741243e52c9a14
Marvin Arnold <marvin@geeky.rocks>
2021-10-18 14:55:45 -0500

    fix: various concerns
    - repackage logger from hm_pyhelper.utils to hm_pyhelper.logger
    - LOGGER should be instantiated as uppercase by convention
    - add tests
    - add retry logic to get_region
    - craete await_spi_available helper

--- hm_pyhelper/tests/test_miner_param.py
+++ hm_pyhelper/tests/test_miner_param.py
@@ -1,8 +1,9 @@
+from hm_pyhelper.miner_param import retry_get_region, await_spi_available, \
+    provision_key, did_gateway_mfr_test_result_include_miner_key_pass, \
+    REGION_OVERRIDE_KEY
 import unittest
-from unittest.mock import patch
-from hm_pyhelper.miner_param import provision_key
-from hm_pyhelper.miner_param import \
-        did_gateway_mfr_test_result_include_miner_key_pass
+from unittest.mock import mock_open, patch
+import os
 
 ALL_PASS_GATEWAY_MFR_TESTS = [
     {
@@ -77,7 +78,6 @@ class GatewayMfrProvisionMock:
 
 
 class TestMinerParam(unittest.TestCase):
-
     @patch(
             'hm_pyhelper.miner_param.get_gateway_mfr_test_result',
             return_value={
@@ -161,3 +161,16 @@ class TestMinerParam(unittest.TestCase):
                     get_gateway_mfr_test_result
                     )
         )
+
+    def test_get_region_from_override(self):
+        os.environ[REGION_OVERRIDE_KEY] = 'foo'
+        self.assertEqual(retry_get_region(REGION_OVERRIDE_KEY, "foo/"), 'foo')
+
+    @patch("builtins.open", new_callable=mock_open, read_data="ZZ111\n")
+    def test_get_region_from_miner(self, _):
+        os.environ[REGION_OVERRIDE_KEY] = ''
+        self.assertEqual(retry_get_region(REGION_OVERRIDE_KEY, "foo/"), 'ZZ111')  # noqa: E501
+
+    @patch("os.path.exists", return_value=True)
+    def test_is_spi_available(self, _):
+        self.assertTrue(await_spi_available("spiXY.Z"))
